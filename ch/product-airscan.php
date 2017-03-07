4aed661eaa3cf37df50e2e0429703a314fa07ef3
jingxian0320 <baojingxian0320@gmail.com>
2017-03-07 22:50:18 +0800

    remove fancybox&root index

--- ch/product-airscan.php
+++ ch/product-airscan.php
@@ -71,8 +71,8 @@
 </div><!-- end-wrap -->
 <!-- End Document
 ================================================== -->
-<script type="text/javascript" src="./fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-<script type="text/javascript" src="./fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<!-- <script type="text/javascript" src="./fancybox/jquery.mousewheel-3.0.4.pack.js"></script> -->
+<!-- <script type="text/javascript" src="./fancybox/jquery.fancybox-1.3.4.pack.js"></script> -->
 <script type="text/javascript">
 
 	$(document).ready(function() {
---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+$lan = substr( $HTTP_ACCEPT_LANGUAGE,0,5);
+if ($lan == "zh-cn")
+print("<meta http-equiv='refresh' content = '0;URL = ch/index.html'>");
+else
+print("<meta http-equiv='refresh' content = '0;URL = en/index.html'>");
+?>
